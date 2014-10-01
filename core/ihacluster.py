3f1e835de26157247de0ad1fa4dfbd502fd2a92e
Pablo Celayes <pablocelayes@gmail.com>
2014-10-01 00:02:52 -0300

    redesigned hierarchy management, progressed with ihac implementation

--- core/ihacluster.py
+++ core/ihacluster.py
@@ -15,15 +15,139 @@ def distance(vec1, vec2):
 
 class IHAClusterer(object):
     def __init__(self):
-        self.nodes = []
-        self.leaves = [] # Indices of leaf nodes
-        self.distances = {} # A dictionary holding cluster center distances for pairs of  node indices
+        self.hierarchy = Hierarchy()
 
-    def get_distance(self, i, j):
-        if not (i, j) in self.distances:
-            ni = self.nodes[i]
-            nj = self.nodes[j]
-            distances[(i, j)] = distance(ni.center, nj.center)
+    def fcluster(self, density_threshold=None):
+        """
+            Creates flat clusters by pruning all clusters
+            with density higher than the given threshold
+            and taking the leaves of the resulting hierarchy
+
+            In case no density_threshold is given,
+            we use the average density accross the entire hierarchy
+        """
+        pass
+
+
+class Hierarchy(object):
+    def __init__(self, size):
+        """
+            Size is the number of points we plan to cluster
+        """
+        self.size
+        m_nodes = 2 * size - 1 # maximum number of nodes in the hierarchy
+        self.nodes = [None] * m_nodes  # a list that will hold all the nodes created
+        self.distances = -1 * np.ones((, 2 * size)) # A matrix holding cluster center distances for pairs of  node indices
+        self.leaves = set() # Indices of leaf nodes
+        self.root = None
+
+    def resize(self):
+        """
+            resize data structures to hold process
+            a new batch of points
+        """
+        pass
+
+    def incorporate(self, vec):
+        new_node = self.hierarchy.create_node(vec)
+        if len(self.hierarchy.leaves) > 1:
+            leaf, d = self.get_closest_leaf(new_node) 
+            host = None
+            node = leaf.parent
+            while node and host is None:
+                nchild, d = node.get_nearest_child(new_node)
+                if d >= node.lower_limit() and d <= node.upper_limit():
+                    self.ins_node(node, new_node)
+                    host = node
+                elif d < node.lower_limit:
+                    # • if N J forms a higher dense region on N , 
+                    # and N J forms a lower dense region on at least one of N ‘s
+                    # child nodes 
+                    dist = None
+                    for ch in node.children:
+                        if self.forms_lower_dense_region(new_node, ch):
+                            break
+                            if dist is None or cdist < dist:
+                                dist = cdist
+                                host = ch
+                    # then perform INS HIERARCHY (N I , N J ) 
+                    # where N I is the child node of N closest to the new point N J .
+
+                    # QUESTION: is N I supposed to be chosen among those child nodes
+                    # for which N J forms a lower dense region?
+                    if host:
+                        self.ins_hierarchy(host, new_node)
+
+            if host is None:
+                self.ins_hierarchy(self.root, new_node)
+
+            self.restructure_hierarchy()
+
+    def restructure_hierarchy(self, host_node):
+        """Algorithm Hierarchy Restructuring
+            starting on host_node, we traverse ancestors doing
+            the following:
+            1. Recover the siblings of current that are misplaced.
+                (A node N J is misplaced as N I ’s sibling iff
+                N J does not form a lower dense region in N I )
+                In such case we apply DEMOTE(N I , N J )
+
+            2. Maintain the homogeneity of crntNode.
+        """
+        current = host_node
+        while current:
+            parent = current.parent
+            siblings = parent.children
+            misplaced = [s for s in siblings if not self.forms_lower_dense_region(s, current)]
+            for node in misplaced:
+                self.demote(current, node)
+
+            self.repair_homogeneity(current)
+            current = parent
+    
+    def repair_homogeneity(self, node):
+        # Algorithm Homogeneity Maintenance(N )
+        # 1. Let an input N be the node that is being examined.
+        # 2. Repeat
+        # 3. Let N I and N J be the pair of neighbors among N ‘s
+        # child nodes with the smallest nearest distance.
+        # 4. If N I and N J form a higher dense region,
+        # 5. Then MERGE (N I , N J ) (see Figure 1d)
+        # 6. Until there is no higher dense region found in N during
+        # the last iteration.
+        # 7. Let M I and M J be the pair of neighbors among N ‘s
+        # child nodes with the largest nearest distance.
+        # 8. If M I and M J form a lower dense region in N ,
+        # 9. Then Let (N I , N J ) = SPLIT (Θ, N ). (see Figure 1e)
+        # 10. Call Homogeneity Maintenance(N I ).
+        # 11. Call Homogeneity Maintenance(N J ).
+        pass
+
+    #
+    # Distance functions
+    #
+    def get_distance(self, ni, nj):
+        if self.distances[i, j] < 0:
+            self.distances[ni.id, nj.id] = distance(ni.center, nj.center)
+        return self.distances[i, j]
+
+    def get_closest_leaf(self, node):
+        """
+            returns closest leaf node and the distance to it
+        """
+        mdist = np.inf
+        cleaf = None
+        for i in self.leaves:
+            leaf = self.nodes[i]
+            dist = self.get_distance(leaf, node)
+            if dist < mdist:
+                mdist = dist
+                cleaf = leaf
+
+        return leaf, mdist
+
+    def get_nearest_child(self, parent, node):
+        return nchild, d
 
     def forms_lower_dense_region(self, a, c):
         """
@@ -47,140 +171,15 @@ class IHAClusterer(object):
 
 
 
-    def incorporate(self, vec):
-        if not self.nodes:
-            self.nodes = [Node(vec)]
-            self.leaves = 0
-        else:
-            # find closest leaf node
-            node, d = self.get_closest_leaf(vec) 
-            found = False
-
-            # Bottom-up host node search:
-            # Starting from the parent of the closest leaf node, per-
-            # form upward search to locate a cluster (or create a new
-            # cluster hierarchy) that can host the new point with min-
-            # imal density changes and minimal disruption of the hi-
-            # erarchy monotonicity.
-            host = None
-            while node and host is None:
-                node = self.get_parent(node)
-                # Let N be the node being examined at current level.
-                # The placement of a new point N J in the hierarchy
-                # is performed according to the following rules:
-                
-                if d >= node.density_lower_limit() and\
-                    d <= node.density_upper_limit():
-                    # • if L L ≤ d ≤ U L then perform INS NODE
-                    # (N, N J ) (see Figure 1a) where d is the distance
-                    # from the new point N J to the nearest N ’s child
-                    # node.
-
-                    self.ins_node(node, Node(vec))
-                    host = node
-                elif d < node.density_lower_limit:
-                    # • if N J forms a higher dense region on N , 
-                    # and N J forms a lower dense region on at least one of N ‘s
-                    # child nodes 
-                    dist = None
-                    for ch in node.children:
-                        cdist = euclidean(vec, ch.center)
-                        if cdist > ch.density_upper_limit():
-                            if dist is None or cdist < dist:
-                                dist = cdist
-                                host = c
-                    # then perform INS HIERARCHY (N I , N J ) 
-                    # where N I is the child node of N closest to the new point N J .
-                    if host:
-                        found = True
-                        self.ins_hierarchy(host, Node(vec))
-                    # If none of the rules applies, the search proceeds to the
-                    # next higher-level cluster.
-
-            # If the search process reaches the top-level cluster,
-            # a new cluster will be inserted at
-            # the top level using the hierarchy insertion operator.
-            if host is None:
-                self.ins_hierarchy(self.root, Node(vec))
-
-            # hierarchy restructuring
-            self.restructure_hierarchy()
-
-    def restructure_hierarchy(self, host_node):
-        """Algorithm Hierarchy Restructuring
-        """
-        current = host_node
-        while current:
-            parent = current.parent
-            # Recover the siblings of current that are misplaced.
-            # 
-            # One of the most common problems is that a node is
-            # stranded at an upper level cluster. In such a case,
-            #  a node N J , which is supposed to be a child node of N I,
-            #  is misplaced as N I ‘s sibling.
-            #
-            # A node N J , which is the sibling of N I , is said to be misplaced as
-            # N I ’s sibling if and only if N J does not form a lower dense
-            # region in N I .
-            #
-            # If such a problem is detected, we iteratively apply DEMOTE(N I , N J )
-            siblings = parent.children
-            misplaced = [s for s in siblings if not self.forms_lower_dense_region(s, current)]
-            for node in misplaced:
-                self.demote(current, node)
-
-            # 5. Maintain the homogeneity of crntNode.
-            self.repair_homogeneity(current)
-            current = parent
-    
-    def repair_homogeneity(self, node):
-        # Algorithm Homogeneity Maintenance(N )
-        # 1. Let an input N be the node that is being examined.
-        # 2. Repeat
-        # 3. Let N I and N J be the pair of neighbors among N ‘s
-        # child nodes with the smallest nearest distance.
-        # 4. If N I and N J form a higher dense region,
-        # 5. Then MERGE (N I , N J ) (see Figure 1d)
-        # 6. Until there is no higher dense region found in N during
-        # the last iteration.
-        # 7. Let M I and M J be the pair of neighbors among N ‘s
-        # child nodes with the largest nearest distance.
-        # 8. If M I and M J form a lower dense region in N ,
-        # 9. Then Let (N I , N J ) = SPLIT (Θ, N ). (see Figure 1e)
-        # 10. Call Homogeneity Maintenance(N I ).
-        # 11. Call Homogeneity Maintenance(N J ).
-        pass
-
-    def get_closest_leaf(self, vec):
-        """
-            returns closest leaf node and the distance to it
-        """
-        # if distance matrix dimension is bigger than
-        # current number of leaves, it means we preloaded
-        # distance matrix for the points we are incorporating
-        # (they are assumed to arrive in the same order)
-
-        # otherwise, we must enlarge the distance matrix with
-        # distances for the new point
-
-    def get_parent(self, node):
-        return self.nodes(node.parent_index)
-
-    def fcluster(self, density_threshold=None):
-        """
-            Creates flat clusters by pruning all clusters
-            with density higher than the given threshold
-            and taking the leaves of the resulting hierarchy
-
-            In case no density_threshold is given,
-            we use the average density accross the entire hierarchy
-        """
-        pass
-
+    #
+    # Restructuring operators
+    #
     def ins_node(self, ni, nj):
+        # Mariano
         pass
 
     def ins_hierarchy(self, ni, nj):
+        # Mariano
         pass
 
     def demote(self, ni, nj):
@@ -193,9 +192,8 @@ class IHAClusterer(object):
         pass
 
 
-
 class Node(object):
-    def __init__(self, vec=None, children=[], parent_index=None):
+    def __init__(self, vec=None, parent=None):
         self.center = vec
         self.children = children
         if children:
@@ -203,9 +201,8 @@ class Node(object):
             # TODO: get distances
             # a list of tuples (index of nearest sibling, distance)
             self.distances = []
-
-        self.index = None
-        self.parent_index = parent_index
+        self.id = None
+        self.parent = parent
         self.mu = None
         self.sigma = None
          
@@ -223,17 +220,16 @@ class Node(object):
         # TODO: update distances
         del self.children[index]
 
-    def density_lower_limit(self):
+    def lower_limit(self):
         n = len(self.children)
         if n > 2:
             return mu - sigma
         else:
             return (2.0 / 3) * self.distances[0][0]
 
-    def density_upper_limit(self):
+    def upper_limit(self):
         n = len(self.children)
         if n > 2:
             return mu + sigma
         else:
             return 1.5 * self.distances[0][0]
-
