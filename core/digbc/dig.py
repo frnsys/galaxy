e797d1b44e54929619df8a3c959d6e8f2fdb1c81
Pablo Celayes <pablocelayes@gmail.com>
2014-10-31 20:48:40 -0300

    implemented and tested DIG structure

--- core/digbc/dig.py
+++ core/digbc/dig.py
@@ -1,74 +1,79 @@
 import networkx as nx
 
-def sentence_split(plain_text):
-    pass
+def sentencize(plain_text):
+    return plain_text.split(".")
 
+def tokenize(sentence):
+    terms = sentence.strip().split(" ")
+    return terms
 
-class DocumentIndexGraph():
-"""
+class DocumentIndexGraph(nx.Graph):
+    """
     Document Index Graph structure
     as defined on the paper
 
     "Efficient Phrase-Based Document Indexing for Web Document Clustering"
-"""
+    """
     def __init__(self):
-        self.document_ids = []
-        self.term_dict = {} # a dictionary mapping terms to graph node ids
-        self.graph = nx.Graph()
+        super(DocumentIndexGraph, self).__init__()
+        self.document_tables = {}
+        self.next_doc_id = 1
 
 
     def index_document(self, plain_text):
+        doc_id = self.next_doc_id
         # split in sentences
-        sentences = sentence_split(plain_text)
+        sentences = sentencize(plain_text)
         # tokenize sentences
         sentences = [tokenize(s) for s in sentences]
 
-        for sent in sentences:
-            prev = sent[0]
-            for term in sent[1:]:
-                # add term to graph if not present
+        for n, sent in enumerate(sentences):
+            previous_term = sent[0]
+            for p, current_term in enumerate(sent[1:]):
+                self.add_edge(doc_id, n + 1, p + 1, previous_term, current_term)
+                previous_term = current_term
 
-    def add_term_node(self, term):
-        pass
-
-
+        self.next_doc_id += 1
 
 
-class TermNode():
-    """
-        Represents a word node in the DocumentIndexGraph
-        It is created the first time the word is seen
-        on a document
-    """
-    def __init__(self, term, doc_id):
-        self.term = term
-        self.document_table = {doc_id: }
+    def add_edge(self, doc_id, sentence_number, position_in_sentence, term1, term2):
+        edge = (term1, term2)
+        super(DocumentIndexGraph, self).add_edge(*edge)
+        self.document_tables.setdefault(term1, {})
+        doc_table = self.document_tables[term1]
 
+        doc_table.setdefault(doc_id, {})
+        edge_table = doc_table[doc_id]
 
-class DocumentTableEntry(object):
-    """docstring for  """
-    def __init__(self, doc_id):
-        self.doc_id = doc_id
-        self.term_frequencies = {'low': 0, 'med': 0, 'high': 0}
-        self.edge_table = {}
+        edge_table.setdefault(edge, {})
+        edge_table[edge][sentence_number] = position_in_sentence
 
-    def add_edge(self, edge_id, sentence_n, pos):
-        """
-            Records the fact that given edge
-            occurs on sentence_n of the represented
-            document on position pos
-        """
-        self.edge_table.setdefault(edge_id, {})
-        self.edge_table[edge_id][sentence_n] = pos
+if __name__ == '__main__':
+    docs = ["river rafting. mild river rafting. river rafting trips",
+            "wild river adventures. river rafting vacation plan",
+            "fishin trips. fishing vacation plan. booking fishing trips. river fishing"]
 
+    dig = DocumentIndexGraph()
+    for doc in docs:
+        dig.index_document(doc)
 
+    import ipdb; ipdb.set_trace()
 
-class Document():
-    """
-    Phrase-based document structure
-    as defined on the paper
+    # TODO: turn the following session into a test
+    # (this is the example from the figure 3 in the paper,
+    #  we use 1-based indices for docs, sentences, etc. to make
+    #  make it easier to match results with the paper)
 
-    "Efficient Phrase-Based Document Indexing for Web Document Clustering"
-    """
-    def __init__(self, plain_text):
-
+    # ipdb> dig.nodes()
+    # ['river', 'vacation', 'booking', 'rafting', 'fishing', 'trips', 'plan', 'fishin', 'adventures', 'wild', 'mild']
+    # ipdb> dig.edges()
+    # [('river', 'adventures'), ('river', 'fishing'), ('river', 'rafting'), ('river', 'wild'), ('river', 'mild'), ('vacation', 'rafting'), ('vacation', 'fishing'), ('vacation', 'plan'), ('booking', 'fishing'), ('rafting', 'trips'), ('fishing', 'trips'), ('trips', 'fishin')]
+    # ipdb> dig.document_tables["river"]
+    # {1: {('river', 'rafting'): {1: 1, 2: 2, 3: 1}}, 2: {('river', 'rafting'): {2: 1}, ('river', 'adventures'): {1: 2}}, 3: {('river', 'fishing'): {4: 1}}}
+    # ipdb> doc_table = dig.document_tables["river"]
+    # ipdb> doc_table[1]
+    # {('river', 'rafting'): {1: 1, 2: 2, 3: 1}}
+    # ipdb> doc_table[2]
+    # {('river', 'rafting'): {2: 1}, ('river', 'adventures'): {1: 2}}
+    # ipdb> doc_table[3]
+    # {('river', 'fishing'): {4: 1}}
