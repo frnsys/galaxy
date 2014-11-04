c18af5e7eeeefbdb85f06f784c7a17aee2c9d0d7
Pablo Celayes <pablocelayes@gmail.com>
2014-11-04 07:48:29 -0300

    added term frequencies by level

--- core/digbc/dig.py
+++ core/digbc/dig.py
@@ -1,8 +1,16 @@
 import networkx as nx
+import numpy as np
 from copy import copy
 
+# Level Definitions
+HIGH = 0
+MED = 1
+LOW = 2
+
 def sentencize(plain_text):
-    return plain_text.split(".")
+    sentences = plain_text.split(".")
+    sentences = [tokenize(s) for s in sentences]
+    return [RankedSentence(sentence=s, level=LOW) for s in sentences]
 
 def tokenize(sentence):
     terms = sentence.strip().split(" ")
@@ -25,33 +33,36 @@ class DocumentIndexGraph(nx.DiGraph):
     def index_document(self, plain_text):
         doc_id = self.next_doc_id
 
-        # split in sentences
+        # parse ranked tokenized sentences
         sentences = sentencize(plain_text)
-        # tokenize sentences
-        sentences = [tokenize(s) for s in sentences]
 
-        for n, sent in enumerate(sentences):
+        for n, rsent in enumerate(sentences):
+            sent, level = rsent.sentence, rsent.level
             previous_term = sent[0]
             for p, current_term in enumerate(sent[1:]):
                 position = (n + 1, p + 1)
-                self.add_edge(doc_id, position, previous_term, current_term)
+                self.add_edge(doc_id, position, previous_term, current_term, level)
                 previous_term = current_term
+            # update term freq for last term (add edge only updates for startig term of
+            #    given pair)
+            doc_table = self.get_doc_table(sent[-1])
+            doc_table.setdefault(doc_id, DocumentTableEntry())
+            doc_table[doc_id].term_freqs[level] += 1
 
         self.next_doc_id += 1
 
-    def add_edge(self, doc_id, position, term1, term2):
+    def add_edge(self, doc_id, position, term1, term2, level):
         # position is a tuple (sent_n, term_n) indicating
         # the sentence number and term number where the occurence was found
         edge = (term1, term2)
         super(DocumentIndexGraph, self).add_edge(*edge)
-        self.document_tables.setdefault(term1, {})
-        doc_table = self.document_tables[term1]
+        doc_table = self.get_doc_table(term1)
 
         # retrieve list of docs sharing this edge
         # and update phrase matches
-        matching_doc_ids = [d_id for d_id in doc_table.keys() if edge in doc_table[d_id]]
+        matching_doc_ids = [d_id for d_id in doc_table.keys() if edge in doc_table[d_id].edge_table]
         for d_id in matching_doc_ids:
-            d_edge_table = doc_table[d_id]
+            d_edge_table = doc_table[d_id].edge_table
             # occurences of current edge in doc d
             d_edge_positions = copy(d_edge_table[edge])
 
@@ -71,11 +82,13 @@ class DocumentIndexGraph(nx.DiGraph):
                 self.get_matching_phrases(d_id, doc_id).append(new_pmatch)
 
         # update document table for term1 with current doc
-        doc_table.setdefault(doc_id, {})
-        edge_table = doc_table[doc_id]
+        doc_table.setdefault(doc_id, DocumentTableEntry())
+        edge_table = doc_table[doc_id].edge_table
 
         edge_table.setdefault(edge, [])
         edge_table[edge].append(position)
+
+        doc_table[doc_id].term_freqs[level] += 1
 
 
     def get_matching_phrases(self, doc_a_id, doc_b_id):
@@ -84,8 +97,26 @@ class DocumentIndexGraph(nx.DiGraph):
 
         return self.matching_phrases[ordered_ids]
 
+    def get_doc_table(self, term):
+        self.document_tables.setdefault(term, {})
+        return self.document_tables[term]
 
 
+
+
+class DocumentTableEntry(object):
+    """docstring for DocumentTableEntry"""
+    def __init__(self):
+        self.term_freqs = [0, 0, 0]
+        self.edge_table = {}
+
+
+
+class RankedSentence(object):
+    """docstring for RankedSentence"""
+    def __init__(self, sentence, level):
+        self.sentence = sentence
+        self.level = level
 
 
 class PhraseMatch(object):
